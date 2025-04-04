5a7cc158aa3c5cf0616fb2728ecb2ebd755a9df6
Diamondman32 <samnolte0302@gmail.com>
2025-04-04 18:47:07 -0500

    first optimization :)

--- baseline.cpp
+++ baseline.cpp
@@ -2,23 +2,33 @@
 #include <iomanip>
 #include <chrono>
 
-int main() {
-    // Start time
-    auto start = std::chrono::high_resolution_clock::now();
+static unsigned long long UPPER_BOUND = 5*1e10;
 
-    // Do test
-    for(unsigned long long i = 10e9; i <= 5*10e9; i++)
+int main() {
+    std::cout << "Testing numbers from 1 to " << UPPER_BOUND << "\nTesting...\n";
+    std::chrono::_V2::system_clock::time_point start = std::chrono::high_resolution_clock::now();
+
+    // Test
+    for(unsigned long long currentCount = 1; currentCount <= UPPER_BOUND; currentCount++)
     {
-        unsigned long long num = i;
-        while(num != 1)
-            if(num%2 == 0)
-                num /= 2;
+        unsigned long long manipulatedCount = currentCount;
+        while(manipulatedCount != 1)
+        {
+            if(manipulatedCount % 2 == 0)
+                manipulatedCount /= 2;
             else
-                num = num*3 + 1;
+            {
+                manipulatedCount = manipulatedCount * 3 + 1;
+                manipulatedCount /= 2;
+            }
+
+            // Numbers below currentCount have already been proved to be magic
+            if(currentCount > manipulatedCount)
+                break;
+        }
     }
 
-    // End time
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::seconds>(end-start);
-    std::cout << std::fixed << std::setprecision(3) << "The test took approximately " << duration.count() << " microseconds";
+    std::chrono::_V2::system_clock::time_point end = std::chrono::high_resolution_clock::now();
+    double duration = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count() / 1e3;
+    std::cout << std::fixed << std::setprecision(3) << "The test took approximately " << duration << " seconds";
 }
