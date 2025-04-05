c8164d657d11c9dd4e7e056b09e8feecd6e1eb6e
Diamondman32 <samnolte0302@gmail.com>
2025-04-04 23:46:20 -0500

    3rd optimization

--- baseline.cpp
+++ baseline.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <iomanip>
 #include <chrono>
+#include <vector>
+#include <thread>
 
 static unsigned long long UPPER_BOUND = 5*1e9;
+static int NUM_THREADS = 15;
 
-int main() {
-    std::cout << "Testing numbers from 1 to " << UPPER_BOUND << "\nTesting...\n";
-    std::chrono::_V2::system_clock::time_point start = std::chrono::high_resolution_clock::now();
-
-    // Test
-    for(unsigned long long currentCount = 1; currentCount <= UPPER_BOUND; ++currentCount)
+bool VerifyMagic(unsigned long long currentCount, unsigned long long upperBound)
+{
+    unsigned long long startingCount = currentCount;
+    for(currentCount; currentCount <= upperBound; ++currentCount)
     {
+        if(currentCount == upperBound)
+            break;
         unsigned long long manipulatedCount = currentCount;
         while(manipulatedCount != 1)
         {
@@ -22,10 +25,25 @@ int main() {
                 manipulatedCount >>= 1;
             }
 
-            // Numbers below currentCount have already been proved to be magic
+            // RESTRICTIVE CONDITION -->  if(currentCount > manipulatedCount && manipulatedCount >= startingCount)
             if(currentCount > manipulatedCount)
                 break;
         }
+    }
+    return true;
+}
+
+int main() {
+    std::cout << "Testing numbers from 1 to " << UPPER_BOUND << "\nTesting...\n";
+    std::chrono::_V2::system_clock::time_point start = std::chrono::high_resolution_clock::now();
+    std::vector<std::thread> threads;
+    
+    // Assign Tests
+    for(int i = 0; i < NUM_THREADS; ++i)
+        threads.emplace_back(VerifyMagic, UPPER_BOUND/NUM_THREADS*i+1, UPPER_BOUND/NUM_THREADS*(i+1));
+
+    for (std::thread& th : threads) {
+        th.join();
     }
 
     std::chrono::_V2::system_clock::time_point end = std::chrono::high_resolution_clock::now();
